37126c76679953110f9a789bab094e302877a304
dChunikhin <dchunikhin95@gmail.com>
2020-06-08 23:13:21 +0300

    Updates

---
+++ precache-manifest.0f91f218d733709b945b28df92a402c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d2b8b6a1b9e75c10bdb4920b57b737d",
+    "url": "/checkout-page-idr/static/media/parfum.5d2b8b6a.png"
+  },
+  {
+    "revision": "b09f8bd58a623a33bcba",
+    "url": "/checkout-page-idr/static/js/runtime~main.29b4df0e.js"
+  },
+  {
+    "revision": "d2d3ef102d2cabf80e1c",
+    "url": "/checkout-page-idr/static/js/main.a0f70576.chunk.js"
+  },
+  {
+    "revision": "7259446daad2d2a6c244",
+    "url": "/checkout-page-idr/static/js/2.d22af375.chunk.js"
+  },
+  {
+    "revision": "20e7533b70f73518dc207cf0ea99d43b",
+    "url": "/checkout-page-idr/index.html"
+  }
+];
